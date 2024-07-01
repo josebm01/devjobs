1053a794c8413627b19f40d1d9b85fdeb190e3be
Jose BM <jbaxin@manuelsolis.com>
2024-07-01 17:09:39 -0600

    Se crea componente livewire de formulario para crear una vacante

--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,3 +1,81 @@
-<div>
-    <h1>Livewire</h1>
-</div>
+<form action="" class="md:w-1/2 space-y-5">
+
+    <div>
+        <x-input-label for="titulo" :value="__('Título vacante')" />
+        <x-text-input 
+            id="titulo" 
+            name="titulo" 
+            class="block mt-1 w-full" 
+            type="text" 
+            :value="old('titulo')" 
+        />
+        <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="salario" :value="__('Salario mensual')" />
+        <select name="salario" id="salario" class="w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring focus:rinf-indigo-200 focus:ring-opacity-50">
+        </select>
+        <x-input-error :messages="$errors->get('salario')" class="mt-2" />
+    </div>
+    
+    <div>
+        <x-input-label for="categoria" :value="__('Categoría')" />
+        <select name="categoria" id="categoria" class="w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring focus:rinf-indigo-200 focus:ring-opacity-50">
+        </select>
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+    
+    <div>
+        <x-input-label for="empresa" :value="__('Empresa')" />
+        <x-text-input 
+            id="empresa" 
+            name="empresa" 
+            class="block mt-1 w-full" 
+            type="text" 
+            :value="old('empresa')"
+            placeholder="Ej. Netflix, Uber, Shopify" 
+        />
+        <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
+    </div>
+    
+    <div>
+        <x-input-label for="ultimo_dia" :value="__('Último día para postularse')" />
+        <x-text-input 
+            id="ultimo_dia" 
+            name="ultimo_dia" 
+            class="block mt-1 w-full" 
+            type="date" 
+            :value="old('ultimo_dia')"
+        />
+        <x-input-error :messages="$errors->get('ultimo_dia')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="descripcion" :value="__('Descripción del puesto')" />
+        <textarea 
+            id="descripcion" 
+            rows="5"
+            name="descripcion" 
+            placeholder="Descripción general del puesto"
+            class="w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring focus:rinf-indigo-200 focus:ring-opacity-50"
+        ></textarea>
+        <x-input-error :messages="$errors->get('descripcion')" class="mt-2" />
+    </div>
+
+    <div>
+        <x-input-label for="imagen" :value="__('Imagen')" />
+        <x-text-input 
+            id="imagen" 
+            name="imagen"
+            class="block mt-1 w-full" 
+            type="file" 
+        />
+        <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
+    </div>
+
+    <x-primary-button class="w-full justify-center">
+        {{ __('Crear vacante') }}
+    </x-primary-button>
+
+</form>
